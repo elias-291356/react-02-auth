6afb38a2c01f2b9dfd182619f608bd1ddfc3c242
elias-291356 <ikucher.002@gmail.com>
2025-06-29 09:39:52 +0200

    routes UI-auth tailwind

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from "react-router-dom"
+
+const Navbar = () => {
+  return (
+      <ul>
+          <li>
+              <NavLink to='/'>Home</NavLink>
+          </li>
+          <li>
+              <NavLink to='/todoList'>Todo List</NavLink>
+          </li>
+          <li>
+              <NavLink to='/login'>Login</NavLink>
+          </li>
+          <li>
+              <NavLink to='/register'>Register</NavLink>
+          </li>
+    </ul>
+  )
+}
+
+export default Navbar
+
+
