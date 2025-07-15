b1761e291f34b0509396fd9e631de7c872122016
elias-291356 <ikucher.002@gmail.com>
2025-07-15 16:48:37 +0200

    pp

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -3,80 +3,72 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import * as api from "../../shared/api/auth";
 
 export const registerThunk = createAsyncThunk(
-    "auth/register",
-    async (data, { rejectWithValue }) => {
-        try {
-            const result = await api.register(data);
-            return result;
-        }
-        catch ({ response }) {
-            console.log(response)
-            return rejectWithValue(response.data.message);
-        }
+  "auth/register",
+  async (data, { rejectWithValue }) => {
+    try {
+      const result = await api.register(data);
+      return result;
+    } catch ({ response }) {
+      console.log(response);
+      return rejectWithValue(response.data.message);
     }
-)
+  },
+);
 
 export const loginThunk = createAsyncThunk(
-    "auth/login",
-    async (data, { rejectWithValue }) => {
-        try {
-            const result = await api.login(data);
-            return result;
-        }
-        catch ({ response }) {
-            return rejectWithValue(response.data.message);
-        }
+  "auth/login",
+  async (data, { rejectWithValue }) => {
+    try {
+      const result = await api.login(data);
+      return result;
+    } catch ({ response }) {
+      return rejectWithValue(response.data.message);
     }
-)
+  },
+);
 
 export const logoutThunk = createAsyncThunk(
-    "auth/logout",
-    async (_, { rejectWithValue }) => {
-        try {
-            const result = await api.logout();
-            return result;
-        }
-        catch ({ response }) {
-            return rejectWithValue(response.data.message);
-        }
+  "auth/signout",
+  async (_, { rejectWithValue }) => {
+    try {
+      const result = await api.logout();
+      return result;
+    } catch ({ response }) {
+      return rejectWithValue(response.data.message);
     }
-)
+  },
+);
 
 export const currentThunk = createAsyncThunk(
-    "auth/current",
-    async (_, { rejectWithValue, getState }) => {
-        try {
-            const { auth } = getState();
-            const result = await api.getCurrent(auth.token);
-            return result;
-        }
-        catch ({ response }) {
-            return rejectWithValue(response.data.message);
-        }
-    },
-    {
-        condition: (_, { getState }) => {
-            const {auth} = getState();
-            if(!auth.token) {
-                return false;
-            }
-        }
+  "auth/current",
+  async (_, { rejectWithValue, getState }) => {
+    try {
+      const { auth } = getState();
+      const result = await api.getCurrent(auth.token);
+      return result;
+    } catch ({ response }) {
+      return rejectWithValue(response.data.message);
     }
-)
+  },
+  {
+    condition: (_, { getState }) => {
+      const { auth } = getState();
+      if (!auth.token) {
+        return false;
+      }
+    },
+  },
+);
 
-
-
-export const refreshThunk = createAsyncThunk('refresh', async (_, thunkApi) => {
-	const savedToken = thunkApi.getState().auth.token;
-	if (!savedToken) {
-		return thunkApi.rejectWithValue('Token is not exist');
-	}
-	try {
-		const data = await api.getCurrent(savedToken);
-		return data;
-	} catch (error) {
-		return thunkApi.rejectWithValue(error.message);
-	}
+export const refreshThunk = createAsyncThunk("refresh", async (_, thunkApi) => {
+  const savedToken = thunkApi.getState().auth.token;
+  if (!savedToken) {
+    return thunkApi.rejectWithValue("Token is not exist");
+  }
+  try {
+    const data = await api.getCurrent(savedToken);
+    return data;
+  } catch (error) {
+    return thunkApi.rejectWithValue(error.message);
+  }
 });
-
-
